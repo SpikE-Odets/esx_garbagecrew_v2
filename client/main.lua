ec5721ae0bb6f0faf5b60c013c2d77e4209c0d17
SpikE-Odets <51069593+SpikE-Odets@users.noreply.github.com>
2020-02-01 20:54:05 -0500

    Add files via upload

--- client/main.lua
+++ client/main.lua
@@ -1,7 +1,9 @@
 ESX = nil
 local AreaType, AreaMarker, AreaInfo, currentZone = nil, nil, nil, nil
 local HasAlreadyEnteredArea, clockedin, vehiclespawned = false, false, false
-local work_truck = nil
+local work_truck, NewDrop, LastDrop = nil, nil, nil
+local Blips = {}
+
 
 Citizen.CreateThread(function()
 	while ESX == nil do
@@ -29,26 +31,43 @@ AddEventHandler('esx:setJob', function(job)
 	PlayerData.job = job
 end)
 
+RegisterNetEvent('esxgarbagejob:movetruckcount')
+AddEventHandler('esxgarbagejob:movetruckcount', function(count)
+	Config.TruckPlateNumb = count
+end)
+
+
 RegisterNetEvent('esx_garbagecrew:enteredarea')
 AddEventHandler('esx_garbagecrew:enteredarea', function(zone)
-	print('entered area')
 	lastZone = zone  
 	CurrentAction = zone.name
-	print(CurrentAction)
+
 	if CurrentAction == 'timeclock' then
 		MenuCloakRoom()
 	end
 
 	if CurrentAction == 'vehiclelist' then
-		print('Entered Vehicle List Marker - Clocked In ('..tostring(clockedin) .. ')')
 		if clockedin  then
 			MenuVehicleSpawner()
 		end
 	end
 
 	if CurrentAction == 'endmission' then
-		print('entered the end mission marker')
 		CurrentActionMsg = _U('cancel_mission')
+	end
+
+	if CurrentAction == 'collection' then
+		if IsPedInAnyVehicle(GetPlayerPed(-1)) and GetVehicleNumberPlateText(GetVehiclePedIsIn(GetPlayerPed(-1), false)) == worktruckplate then
+			CurrentActionMsg = _U('collection')
+		else
+			CurrentActionMsg = _U('need_work_truck')
+		end
+
+	end
+
+	if CurrentAction == 'bags' then
+
+
 	end
 
 end)
@@ -69,7 +88,6 @@ Citizen.CreateThread( function()
 		for i, v in pairs(Config.Zones) do
 			if GetDistanceBetweenCoords(plyloc, v.pos, true)  < 20.0 then
 				sleep = 0
-
 				if v.name == 'timeclock' and IsGarbageJob() then
 					DrawMarker(1, v.pos.x, v.pos.y, v.pos.z, 0.0, 0.0, 0.0, 0, 0.0, 0.0, v.size,  v.size, 1.0, 204,204, 0, 100, false, true, 2, false, false, false, false)
 				elseif v.name == 'endmission' and vehiclespawned then
@@ -81,11 +99,9 @@ Citizen.CreateThread( function()
 		end
 
 		if oncollection then
-			for i, v in pairs(Config.Collections) do
-				if GetDistanceBetweenCoords(plyloc, v.pos, true)  < 20.0 then
-					sleep = 0
-					DrawMarker(1, v.pos, 0, 0, 0, 0, 0, 0, 3.0, 3.0, 1.0, 0, 204, 204, 0, 0, 0, 0, 0)
-				end
+			if GetDistanceBetweenCoords(plyloc, NewDrop.pos, true)  < 20.0 then
+				sleep = 0
+				DrawMarker(1, NewDrop.pos.x,  NewDrop.pos.y,  NewDrop.pos.z, 0.0, 0.0, 0.0, 0, 0.0, 0.0,  NewDrop.size,  NewDrop.size, 1.0, 204,204, 0, 100, false, true, 2, false, false, false, false)
 			end
 		end
 
@@ -108,11 +124,19 @@ Citizen.CreateThread( function()
 				if CurrentAction == 'endmission' then
 					ESX.Game.DeleteVehicle(work_truck)
 					vehiclespawned = false
-					
+					CurrentAction =nil
+					CurrentActionMsg = nil
 				end
 
+				if CurrentAction == 'collection' then
+					if CurrentActionMsg == _U('collection') then
+						SelectBinAndCrew()
+					end
+				end
 
-
+				if CurrentActionMsg == 'bags' then
+					CollectBagFromBin()
+				end
 
 			end
 		end
@@ -135,10 +159,10 @@ Citizen.CreateThread( function()
 			end
 		end
 
-		for i,v in pairs(Config.Collections) do
-			if GetDistanceBetweenCoords(plyloc, v.pos, true)  <  v.size then
-				IsInCollection = true
-				currentZone = v
+		if oncollection then
+			if GetDistanceBetweenCoords(plyloc, NewDrop.pos, true)  <  NewDrop.size then
+				IsInArea = true
+				currentZone = NewDrop
 			end
 		end
 
@@ -160,6 +184,58 @@ Citizen.CreateThread( function()
 	end
 end)
 
+function SelectBinAndCrew()
+	local bin = nil
+	for i, v in pairs(Config.DumpstersAvaialbe) do
+		bin = GetClosestObjectOfType(NewDrop.pos, 5.0, GetHashKey(v), false, false, false )
+		if bin ~= nil then
+			break
+		end
+	end
+	
+	if bin ~= nil then
+		truckplate = GetVehicleNumberPlateText(work_truck)
+		TriggerServerEvent('esx_garbagecrew:setworkers', GetEntityCoords(bin), truckplate )
+	else
+		ESX.ShowNotification('No trash abailable for pickup at this location.')
+		FindDeliveryLoc(LastDrop)
+	end
+end
+
+function FindDeliveryLoc(LastDrop)
+	if LastDrop ~= nil then
+		lastregion = GetNameOfZone(LastDrop.pos)
+	end
+	local newdropregion = nil
+	while newdropregion == nil or newdropregion == lastregion do
+		randomloc = math.random(1, #Config.Collections)
+		newdropregion = GetNameOfZone(Config.Collections[randomloc].pos)
+	end
+	NewDrop = Config.Collections[randomloc]
+	LastDrop = NewDrop
+	if Blips['delivery'] ~= nil then
+		RemoveBlip(Blips['delivery'])
+		Blips['delivery'] = nil
+	end
+	
+	if Blips['endmission'] ~= nil then
+		RemoveBlip(Blips['endmission'])
+		Blips['endmission'] = nil
+	end
+	
+	Blips['delivery'] = AddBlipForCoord(NewDrop.pos)
+	SetBlipRoute(Blips['delivery'], true)
+	BeginTextCommandSetBlipName("STRING")
+	AddTextComponentString(_U('blip_delivery'))
+	EndTextCommandSetBlipName(Blips['delivery'])
+	
+	Blips['endmission'] = AddBlipForCoord(Config.Zones[1].pos)
+	BeginTextCommandSetBlipName("STRING")
+	AddTextComponentString(_U('blip_goal'))
+	EndTextCommandSetBlipName(Blips['endmission'])
+
+	oncollection = true
+end
 
 function IsGarbageJob()
 	if ESX ~= nil then
@@ -257,14 +333,14 @@ function MenuVehicleSpawner()
 			ESX.Game.SpawnVehicle(data.current.value, Config.VehicleSpawn.pos, 270.0, function(vehicle)
 				local trucknumber = Config.TruckPlateNumb + 1
 				if trucknumber <=9 then
-					SetVehicleNumberPlateText(vehicle, 'TCREW00'..trucknumber)
-					plaquevehicule =   'TCREW00'..trucknumber 
+					SetVehicleNumberPlateText(vehicle, 'GCREW00'..trucknumber)
+					worktruckplate =   'GCREW00'..trucknumber 
 				elseif trucknumber <=99 then
-					SetVehicleNumberPlateText(vehicle, 'TCREW0'..trucknumber)
-					plaquevehicule =   'TCREW0'..trucknumber 
+					SetVehicleNumberPlateText(vehicle, 'GCREW0'..trucknumber)
+					worktruckplate =   'GCREW0'..trucknumber 
 				else
-					SetVehicleNumberPlateText(vehicle, 'TCREW'..trucknumber)
-					plaquevehicule =   'TCREW'..trucknumber 
+					SetVehicleNumberPlateText(vehicle, 'GCREW'..trucknumber)
+					worktruckplate =   'GCREW'..trucknumber 
 				end
 
 
@@ -273,6 +349,7 @@ function MenuVehicleSpawner()
 				TaskWarpPedIntoVehicle(GetPlayerPed(-1), vehicle, -1)  
 				vehiclespawned = true 
 				work_truck = vehicle
+				FindDeliveryLoc(LastDrop)
 
 			end)
 
@@ -283,3 +360,18 @@ function MenuVehicleSpawner()
 		end
 	)
 end
+
+
+Citizen.CreateThread(function()
+	local blip = AddBlipForCoord(Config.Zones[2].pos)
+  
+	SetBlipSprite (blip, 318)
+	SetBlipDisplay(blip, 4)
+	SetBlipScale  (blip, 1.2)
+	SetBlipColour (blip, 5)
+	SetBlipAsShortRange(blip, true)
+
+	BeginTextCommandSetBlipName("STRING")
+	AddTextComponentString(_U('blip_job'))
+	EndTextCommandSetBlipName(blip)
+end)
--- server/main.lua
+++ server/main.lua
@@ -2,4 +2,16 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
+RegisterServerEvent('esxgarbagejob:movetruckcount')
+AddEventHandler('esxgarbagejob:movetruckcount', function()
+    Config.TruckPlateNumb = Config.TruckPlateNumb + 1
+    if Config.TruckPlateNumb == 1000 then
+        Config.TruckPlateNumb = 1
+    end
+    TriggerClientEvent('esxgarbagejob:movetruckcount', -1, Config.TruckPlateNumb)
+end)
 
+RegisterServerEvent('esx_garbagejob:setconfig')
+AddEventHandler('esx_garbagejob:setconfig', function()
+    TriggerClientEvent('esxgarbagejob:movetruckcount', -1, Config.TruckPlateNumb)
+end)
