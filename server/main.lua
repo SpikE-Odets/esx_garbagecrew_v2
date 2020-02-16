99777e8809bad24c42ca2f658da3362ecef4e20d
SpikE-Odets <51069593+SpikE-Odets@users.noreply.github.com>
2020-02-16 05:01:48 -0500

    Add files via upload
    Fixed error with multi jobs running at same time.

--- server/main.lua
+++ server/main.lua
@@ -1,67 +1,94 @@
 ESX = nil
 
-local currentjobs, currentadd, currentworkers = {}, {}, {}
+
+local currentjobs = {}
+local currentadd = {}
+local currentworkers = {}
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
+
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(250)
+        local addsleep = 250
+        local collectionfinished = false
+        local updated = false
         while #currentadd > 0 do
-            Citizen.Wait(0)
-            local collectionfinished = false
-            local updated = false
-            for i,v in pairs(currentjobs) do
-                if v.pos == currentadd[1].location and v.trucknumber == currentadd[1].truckplate  then
-                    for workers, ids in pairs(v.workers) do
-                        if ids.id == currentadd[1].id then
-                            ids.bags = ids.bags + 1
+            addsleep = 0
+            if currentadd[1].type == 'bagdumped' then
+                for i,v in pairs(currentjobs) do
+                    if v.pos == currentadd[1].location and v.trucknumber == currentadd[1].truckplate  then
+                        for workers, ids in pairs(v.workers) do
+                            if ids.id == currentadd[1].id then
+                                ids.bags = ids.bags + 1
+                                v.bagsdropped = v.bagsdropped + 1
+                                if v.bagsremaining <= 0  and v.bagsdropped == v.totalbags then
+                                    TriggerEvent('esx_garbagecrew:paycrew', i)
+                                end
+                                updated = true
+                                break
+                            end
+                        end
+
+                        if not updated then
+                            local buildlist = { id = currentadd[1].id, bags = 1,}
+                            table.insert(v.workers, buildlist)
                             v.bagsdropped = v.bagsdropped + 1
                             if v.bagsremaining <= 0  and v.bagsdropped == v.totalbags then
-                                TriggerEvent('esx_garbagecrew:paycrew', i)
+                            TriggerEvent('esx_garbagecrew:paycrew', i)
                             end
-                            updated = true
-                            break
                         end
+                        table.remove(currentadd, 1)
+                    break
                     end
-
-                    if not updated then
-                        local buildlist = { id = currentadd[1].id, bags = 1,}
-                        table.insert(v.workers, buildlist)
-                        v.bagsdropped = v.bagsdropped + 1
-                        if v.bagsremaining <= 0  and v.bagsdropped == v.totalbags then
-                        TriggerEvent('esx_garbagecrew:paycrew', i)
-                        end
-                    end
+                    
                 end
+            elseif currentadd[1].type == 'setworkers' then
+                Citizen.Wait(0)
+                local bagtotal = math.random(Config.MinBags, Config.MaxBags)
+                local buildlist = {type = 'bags', name = 'bagcollection', jobboss = currentadd[1].id, pos = currentadd[1].location, totalbags = bagtotal, bagsdropped = 0, bagsremaining = bagtotal, trucknumber = currentadd[1].trucknumber, truckid = currentadd[1].truckid, workers = {}, }
+                table.insert(currentjobs, buildlist)
+                TriggerClientEvent('esx_garbagecrew:updatejobs', -1, currentjobs)
                 table.remove(currentadd, 1)
+                break
             end
+            Citizen.Wait(addsleep)
         end
-
-        while #currentworkers > 0 do
-            Citizen.Wait(0)
-            local bagtotal = math.random(Config.MinBags, Config.MaxBags)
-            if currentjobs[trucknumber] ~= nil then
-                currentjobs[trucknumber] = nil
-            end
-            local buildlist = {type = 'bags', name = 'bagcollection', jobboss = currentworkers[1].id, pos = currentworkers[1].location, totalbags = bagtotal, bagsdropped = 0, bagsremaining = bagtotal, trucknumber = currentworkers[1].trucknumber, truckid = currentworkers[1].truckid, workers = {}, }
-            table.insert(currentjobs, buildlist)
-            TriggerClientEvent('esx_garbagecrew:updatejobs', -1, currentjobs)
-            table.remove(currentworkers,1)
-        end
+        Citizen.Wait(addsleep)
     end
 end)
+
 
 RegisterServerEvent('esx_garbagecrew:bagdumped')
 AddEventHandler('esx_garbagecrew:bagdumped', function(location, truckplate)
     local _source = source
     local buildlist = {
+        type = 'bagdumped',
         id = _source,
         location = location,
         truckplate = truckplate,
     }
     table.insert(currentadd, buildlist)
 end)
+
+
+RegisterServerEvent('esx_garbagecrew:setworkers')
+AddEventHandler('esx_garbagecrew:setworkers', function(location, trucknumber, truckid)
+    print("trying to set workers")
+    _source = source
+    buildlist = { 
+        type = 'setworkers',
+        id = _source,
+        location = location,
+        trucknumber = trucknumber,
+        truckid = truckid, 
+    }
+   table.insert(currentadd, buildlist)
+   print(tostring(#currentadd))
+end)
+
+
+
 
 RegisterServerEvent('esx_garbagecrew:unknownlocation')
 AddEventHandler('esx_garbagecrew:unknownlocation', function(location, truckplate)
@@ -90,6 +117,7 @@ AddEventHandler('esx_garbagecrew:bagremoval', function(location, trucknumber)
     TriggerClientEvent('esx_garbagecrew:updatejobs', -1, currentjobs)
 end)
 
+
 RegisterServerEvent('esx_garbagecrew:movetruckcount')
 AddEventHandler('esx_garbagecrew:movetruckcount', function()
     Config.TruckPlateNumb = Config.TruckPlateNumb + 1
@@ -107,18 +135,6 @@ AddEventHandler('esx_garbagejob:setconfig', function()
     end
 end)
 
-RegisterServerEvent('esx_garbagecrew:setworkers')
-AddEventHandler('esx_garbagecrew:setworkers', function(location, trucknumber, truckid)
-    _source = source
-    buildlist = { 
-        id = _source,
-        location = location,
-        trucknumber = trucknumber,
-        truckid = truckid, 
-    }
-   table.insert(currentworkers, buildlist)
-end)
-
 AddEventHandler('playerDropped', function()
     _source = source
      for i, v in pairs(currentjobs) do
@@ -130,11 +146,12 @@ AddEventHandler('playerDropped', function()
      end
 end)
 
+
 AddEventHandler('esx_garbagecrew:paycrew', function(number)
     currentcrew = currentjobs[number].workers
     payamount = (Config.StopPay / currentjobs[number].totalbags) + Config.BagPay
     for i, v in pairs(currentcrew) do
-        local xPlayer = ESX.GetPlayerFromId(v.id)
+        local xPlayer = esx.GetPlayerFromId(v.id)
         if xPlayer ~= nil then
             local amount = math.ceil(payamount * v.bags)
             xPlayer.addMoney(tonumber(amount))
